640ed09fc88e099dde56a26249b7cf5e266a7e10
Mikhaylov Moisey <mikhaylov.mya@zolla.com>
2023-02-10 21:03:05 +0300

    init app

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '2540px'
+    }
+  },
+  plugins: [],
+}
